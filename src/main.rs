7485b3fdf09e32be31e80a9a2adf58d024b194fd
PgBiel <9021226+PgBiel@users.noreply.github.com>
2023-02-27 03:58:18 -0300

    initial code

---
+++ src/main.rs
@@ -0,0 +1,37 @@
+use poise::serenity_prelude as serenity;
+use poise::serenity_prelude::GuildId;
+
+struct Data {} // User data, which is stored and accessible in all command invocations
+type Error = Box<dyn std::error::Error + Send + Sync>;
+type Context<'a> = poise::Context<'a, Data, Error>;
+
+/// Displays your or another user's account creation date
+#[poise::command(slash_command)]
+async fn userup(
+    ctx: Context<'_>,
+    #[description = "Selected user"] user: Option<serenity::User>,
+) -> Result<(), Error> {
+    let u = user.as_ref().unwrap_or_else(|| ctx.author());
+    let response = format!("{}'s account was created at {}", u.name, u.created_at());
+    ctx.say(response).await?;
+    Ok(())
+}
+
+#[tokio::main]
+async fn main() {
+    let framework = poise::Framework::builder()
+        .options(poise::FrameworkOptions {
+            commands: vec![userup()],
+            ..Default::default()
+        })
+        .token(std::env::var("DISCORD_TOKEN").expect("missing DISCORD_TOKEN"))
+        .intents(serenity::GatewayIntents::non_privileged())
+        .setup(|ctx, _ready, framework| {
+            Box::pin(async move {
+                poise::builtins::register_in_guild(ctx, &framework.options().commands, GuildId(193903425640071168)).await?;
+                Ok(Data {})
+            })
+        });
+
+    framework.run().await.unwrap();
+}
