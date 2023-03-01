c274934f9ea4d4ed1724dd382162c983180acfa3
PgBiel <9021226+PgBiel@users.noreply.github.com>
2023-03-01 00:09:25 -0300

    userup interactionhandler test

--- src/minirustbot/commands/userup.rs
+++ src/minirustbot/commands/userup.rs
@@ -1,14 +1,18 @@
 use poise::serenity_prelude as serenity;
-use super::super::common::{Context, Error};
+use super::super::common::{ApplicationContext, Error};
 
 /// Displays your or another user's account creation date
 #[poise::command(slash_command)]
 pub async fn userup(
-        ctx: Context<'_>,
+        ctx: ApplicationContext<'_>,
         #[description = "Selected user"] user: Option<serenity::User>,
         ) -> Result<(), Error> {
     let u = user.as_ref().unwrap_or_else(|| ctx.author());
     let response = format!("{}'s account was created at {}", u.name, u.created_at());
+    let id = ctx.interaction.id();
+
+    let rx = ctx.data.interaction_handler()
+        .wait_for_interaction(id.to_string(), serenity::InteractionType::MessageComponent).await;
 
     ctx.send(|create_reply| create_reply
                 .content(response)
@@ -17,9 +21,18 @@ pub async fn userup(
                     create_components.create_action_row(|create_row|
                         create_row.create_button(|create_button|
                             create_button
-                                .custom_id("cool_button")
+                                .custom_id(id)
                                 .label("OK")
                                 .style(serenity::ButtonStyle::Primary))))).await?;
+
+    if let Some(mut rx) = rx {
+        if let Ok(interaction) = rx.changed().await.map(|_| rx.borrow()) {
+            if let Some(serenity::Interaction::MessageComponent(component)) = &*interaction {
+                let response = format!("Wow, nice {}", component.data.custom_id);
+                println!("RESPONSE: {}", response);
+            }
+        }
+    }
     //    ctx.say(response).await?;
     Ok(())
 }
