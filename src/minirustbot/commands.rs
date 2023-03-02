a2a547d1de66fe8f95f22b18897a0380bc1f87e9
PgBiel <9021226+PgBiel@users.noreply.github.com>
2023-03-02 02:11:17 -0300

    add testform testing command

--- src/minirustbot/commands.rs
+++ src/minirustbot/commands.rs
@@ -3,11 +3,13 @@ use super::common::{Data, ErrorBox};
 mod ping;
 mod userup;
 mod modal;
+mod testform;
 
 pub use ping::ping;
 pub use userup::userup;
 pub use modal::modal;
+pub use testform::testform;
 
 pub fn get_commands() -> Vec<poise::Command<Data, ErrorBox>> {
-    vec![ping(), userup(), modal()]
+    vec![ping(), userup(), modal(), testform()]
 }
---
+++ src/minirustbot/commands/testform.rs
@@ -0,0 +1,12 @@
+use super::super::common::{ApplicationContext, ErrorBox};
+use crate::minirustbot::interaction::form::testform::create_test_form;
+
+/// Runs test form
+#[poise::command(slash_command)]
+pub async fn testform(
+        ctx: ApplicationContext<'_>,
+        ) -> Result<(), ErrorBox> {
+    let data = create_test_form().run(ctx).await?;
+    println!("Got testform data: {:?}", data);
+    Ok(())
+}
