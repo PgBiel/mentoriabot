3451f09bb1397b750a712bd1676262cb3f8db1c9
PgBiel <9021226+PgBiel@users.noreply.github.com>
2023-03-10 01:45:38 -0300

    select_option macro: add missing logic

--- macros/src/select_option.rs
+++ macros/src/select_option.rs
@@ -43,7 +43,15 @@ struct SelectOptionAttributes {
     emoji_function: Option<syn::Path>,
 
     /// If this is the default selection option.
-    is_default: bool,
+    is_default: Option<()>,
+}
+
+#[derive(Debug, Clone, darling::FromMeta)]
+#[darling(allow_unknown_fields)]
+struct FieldAttributes {
+    /// The default value for this field when being initialized.
+    /// Otherwise, will default to Default::default().
+    initializer: Option<syn::Expr>,
 }
 
 pub fn select_option(input: syn::DeriveInput) -> Result<TokenStream, darling::Error> {
@@ -73,7 +81,7 @@ pub fn select_option(input: syn::DeriveInput) -> Result<TokenStream, darling::Er
             attrs.value_key = Some(variant.ident.to_string()); // default value key is the variant's
                                                                // name
         }
-        validate_option_attrs(&attrs, &input)?;
+        validate_option_attrs(&attrs, &variant)?;
         variants_and_options.push((variant, attrs));
     }
 
@@ -83,22 +91,24 @@ pub fn select_option(input: syn::DeriveInput) -> Result<TokenStream, darling::Er
     let data_type = enum_attrs.data.clone().unwrap_or(util::empty_tuple_type());
 
     let option_specs = create_select_option_specs(&variants_and_options, &data_type);
-    let from_select_value = from_select_value(&variants_and_options, &data_type);
-    let struct_ident = input.ident;
+    let from_select_value = from_select_value(&variants_and_options, &data_type)?;
+    let enum_ident = input.ident;
     let (impl_generics, ty_generics, where_clause) = input.generics.split_for_impl();
 
     Ok(quote! {
-        impl #impl_generics crate::form::SelectOption<#data_type> for #struct_ident #ty_generics #where_clause {
+        impl #impl_generics crate::form::SelectOption<#data_type> for #enum_ident #ty_generics #where_clause {
             fn get_specs() -> ::std::vec::Vec<crate::form::SelectMenuOptionSpec<#data_type>> {
                 #option_specs
             }
         }
 
         #[::async_trait::async_trait]
-        impl #impl_generics ::core::convert::From<crate::interaction::SelectValue> for #struct_ident #ty_generics #where_clause {
-            fn from(
+        impl #impl_generics ::core::convert::TryFrom<crate::interaction::SelectValue> for #enum_ident #ty_generics #where_clause {
+            type Error = crate::error::Error;
+
+            fn try_from(
                 value: crate::interaction::SelectValue
-            ) -> Self {
+            ) -> crate::error::Result<Self> {
                 #from_select_value
             }
         }
@@ -107,11 +117,11 @@ pub fn select_option(input: syn::DeriveInput) -> Result<TokenStream, darling::Er
 
 fn validate_option_attrs(
     option_attrs: &SelectOptionAttributes,
-    input: &syn::DeriveInput,
+    variant: &syn::Variant,
 ) -> Result<(), darling::Error> {
     if option_attrs.label.is_some() && option_attrs.label_function.is_some() {
         return Err(syn::Error::new(
-            input.ident.span(),
+            variant.ident.span(),
             "Cannot specify #[label] and #[label_function] at the same time.",
         )
         .into());
@@ -119,14 +129,14 @@ fn validate_option_attrs(
 
     if option_attrs.label.is_none() && option_attrs.label_function.is_none() {
         return Err(syn::Error::new(
-            input.ident.span(),
+            variant.ident.span(),
             "Must specify either a #[label] or a #[label_function], so that the menu option may have a label."
         ).into());
     }
 
     if option_attrs.emoji.is_some() && option_attrs.emoji_function.is_some() {
         return Err(syn::Error::new(
-            input.ident.span(),
+            variant.ident.span(),
             "Cannot specify #[emoji] and #[emoji_function] at the same time.",
         )
         .into());
@@ -134,7 +144,7 @@ fn validate_option_attrs(
 
     if option_attrs.description.is_some() && option_attrs.description_function.is_some() {
         return Err(syn::Error::new(
-            input.ident.span(),
+            variant.ident.span(),
             "Cannot specify #[description] and #[description_function] at the same time.",
         )
         .into());
@@ -166,16 +176,17 @@ fn create_select_option_specs(
         let description_function = util::wrap_option_box(description_function);
         let emoji = util::wrap_option_into(emoji);
         let emoji_function = util::wrap_option_box(emoji_function);
+        let is_default = is_default.is_some();
 
         let value_key = value_key.as_ref().expect("Missing value key");
 
         specs.push(quote! {
-            crate::form::SelectMenuOptionSpec<#data_type> {
+            crate::form::SelectMenuOptionSpec::<#data_type> {
                 label: #label,
 
                 label_function: #label_function,
 
-                value_key: #value_key,
+                value_key: crate::interaction::SelectValue(#value_key.into()),
 
                 description: #description,
 
@@ -190,16 +201,73 @@ fn create_select_option_specs(
         });
     }
 
-    quote! {
+    let res = quote! {
         std::vec![
             #( #specs ),*
         ]
-    }
+    };
+    println!("Res = {res}");
+    res
 }
 
 fn from_select_value(
     variants_with_options: &Vec<(&syn::Variant, SelectOptionAttributes)>,
     data_type: &syn::Type,
-) -> TokenStream2 {
-    todo!()
+) -> Result<TokenStream2, darling::Error> {
+    let mut variant_match_arms = Vec::new();
+
+    for (variant, options) in variants_with_options {
+        let variant_name = &variant.ident;
+        let value_key = options
+            .value_key
+            .as_ref()
+            .expect(&*format!("Missing value key for variant '{variant_name}'"));
+        let mut is_named = false;
+        let fields = match &variant.fields {
+            syn::Fields::Named(fields) => {
+                is_named = true;
+                &fields.named
+            }
+            syn::Fields::Unnamed(fields) => &fields.unnamed,
+            syn::Fields::Unit => {
+                variant_match_arms.push(quote! { #value_key => Self::#variant_name });
+                continue;
+            }
+        };
+
+        let mut field_initializers = Vec::new();
+        for field in fields {
+            let attrs: FieldAttributes = util::get_darling_attrs(&field.attrs)?;
+            if let Some(initializer) = attrs.initializer.as_ref() {
+                if is_named {
+                    let field_name = field.ident.as_ref().expect("Expected named field");
+                    field_initializers.push(quote! { #field_name: #initializer });
+                } else {
+                    field_initializers.push(quote! { #initializer });
+                }
+            } else if is_named {
+                let field_name = field.ident.as_ref().expect("Expected named field");
+                field_initializers.push(quote! { #field_name: ::core::default::Default::default() });
+            } else {
+                field_initializers.push(quote! { ::core::default::Default::default() });
+            }
+        }
+
+        variant_match_arms.push(if is_named {
+            quote! { #value_key => Self::#variant_name {
+                #( #field_initializers ),*
+            } }
+        } else {
+            quote! { #value_key => Self::#variant_name( #( #field_initializers ),* ) }
+        });
+    }
+
+    // catch-all: error.
+    variant_match_arms.push(quote! { _ => return ::core::result::Result::Err(crate::error::FormError::InvalidUserResponse.into()) });
+
+    Ok(quote! {
+        ::core::result::Result::Ok(match value.0.as_str() {
+            #( #variant_match_arms ),*
+        })
+    })
 }
