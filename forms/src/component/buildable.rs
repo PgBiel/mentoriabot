cca6623453ce61694d3a47b32c3147cbf3c546e3
PgBiel <9021226+PgBiel@users.noreply.github.com>
2023-03-12 14:41:14 -0300

    forms: add some Buildable auto-impls

--- forms/src/component/buildable.rs
+++ forms/src/component/buildable.rs
@@ -1,3 +1,5 @@
+use poise::serenity_prelude as serenity;
+
 use crate::interaction::HasCustomId;
 
 /// Types implementing this trait can modify builders,
@@ -13,18 +15,43 @@ pub trait Buildable<Builder> {
     ) -> &'a mut Builder;
 }
 
-/// Buildable is automatically implemented for lambdas
-/// with the same signature as `on_build`.
-impl<B, T> Buildable<B> for T where T: Fn(&mut B) -> &mut B {
-    fn on_build<'a>(
-        &self,
-        builder: &'a mut B,
-    ) -> &'a mut B {
-        self(builder)
+macro_rules! implement_buildable_for_lambda_with_type {
+    ($Builder:ty) => {
+        /// [`Buildable`] is automatically implemented for certain lambdas
+        /// with the same signature as `on_build`.
+        impl<T> Buildable<$Builder> for T where T: Fn(&mut $Builder) -> &mut $Builder {
+            fn on_build<'a>(
+                &self,
+                builder: &'a mut $Builder,
+            ) -> &'a mut $Builder {
+                self(builder)
+            }
+        }
     }
 }
 
+implement_buildable_for_lambda_with_type!(serenity::CreateAllowedMentions);
+implement_buildable_for_lambda_with_type!(serenity::CreateEmbed);
+implement_buildable_for_lambda_with_type!(serenity::CreateActionRow);
+implement_buildable_for_lambda_with_type!(serenity::CreateButton);
+implement_buildable_for_lambda_with_type!(serenity::CreateSelectMenu);
+
 /// Union of the [`Buildable`] and [`HasCustomId`] traits.
+/// (Automatically implemented for any trait that implements both of those traits.)
 pub trait BuildableWithId<Builder>: Buildable<Builder> + HasCustomId {}
 
 impl<B, T> BuildableWithId<B> for T where T: Buildable<B> + HasCustomId {}
+
+/// Automatically implement a [`Buildable`] for a Component with a single row
+/// when a type implements `Buildable` for [`serenity::CreateActionRow`].
+impl<T> Buildable<serenity::CreateComponents> for T
+where
+    T: Buildable<serenity::CreateActionRow>
+{
+    fn on_build<'a>(
+        &self,
+        builder: &'a mut serenity::CreateComponents,
+    ) -> &'a mut serenity::CreateComponents {
+        builder.create_action_row(|builder| <Self as Buildable<serenity::CreateActionRow>>::on_build(&self, builder))
+    }
+}
