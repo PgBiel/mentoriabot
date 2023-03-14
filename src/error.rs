f18e754986648ad6377f153f720b66cb31c7aeb4
PgBiel <9021226+PgBiel@users.noreply.github.com>
2023-03-13 23:44:39 -0300

    add diesel errors to Error

--- src/error.rs
+++ src/error.rs
@@ -15,6 +15,12 @@ pub enum Error {
     /// An error occurred in Serenity
     Serenity(serenity::Error),
 
+    /// A Diesel operational error occurred
+    Diesel(diesel::result::Error),
+
+    /// A Diesel connection error occurred
+    DieselConnection(diesel::result::ConnectionError),
+
     /// Some other error
     Other(&'static str),
 }
@@ -39,11 +45,25 @@ impl From<serenity::Error> for Error {
     }
 }
 
+impl From<diesel::result::Error> for Error {
+    fn from(err: diesel::result::Error) -> Self {
+        Self::Diesel(err)
+    }
+}
+
+impl From<diesel::result::ConnectionError> for Error {
+    fn from(err: diesel::result::ConnectionError) -> Self {
+        Self::DieselConnection(err)
+    }
+}
+
 impl Display for Error {
     fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
         match self {
             Self::Form(inner) => Display::fmt(&inner, f),
             Self::EnumParse(inner) => Display::fmt(&inner, f),
+            Self::Diesel(inner) => Display::fmt(&inner, f),
+            Self::DieselConnection(inner) => Display::fmt(&inner, f),
             Self::Serenity(inner) => Display::fmt(&inner, f),
             Self::Other(message) => write!(f, "{}", message),
         }
@@ -56,6 +76,8 @@ impl std::error::Error for Error {
             Self::Form(inner) => Some(inner),
             Self::EnumParse(inner) => Some(inner),
             Self::Serenity(inner) => Some(inner),
+            Self::Diesel(inner) => Some(inner),
+            Self::DieselConnection(inner) => Some(inner),
             _ => None,
         }
     }
