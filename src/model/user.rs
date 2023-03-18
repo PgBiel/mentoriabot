9fe0e63888c1f0645bf3bdd204966e2f80f62e3c
PgBiel <9021226+PgBiel@users.noreply.github.com>
2023-03-17 22:54:48 -0300

    model: merge User and NewUser

--- src/model/user.rs
+++ src/model/user.rs
@@ -4,7 +4,15 @@ use super::DiscordId;
 use crate::schema::*;
 
 /// Represents a registered User of our bot.
-#[derive(Debug, Queryable, Identifiable, Clone, PartialEq, Eq)]
+#[derive(
+    Queryable,
+    Identifiable,
+    Insertable,
+    AsChangeset,
+    Debug,
+    Clone,
+    PartialEq,
+    Eq)]
 #[diesel(primary_key(discord_id))]
 pub struct User {
     pub discord_id: DiscordId,
@@ -12,11 +20,4 @@ pub struct User {
     pub bio: Option<String>,
 }
 
-/// Represents data for a new User in our database.
-#[derive(Debug, Insertable, AsChangeset, Clone, PartialEq, Eq)]
-#[diesel(table_name = users)]
-pub struct NewUser {
-    pub discord_id: DiscordId,
-    pub name: String,
-    pub bio: Option<String>,
-}
+pub type NewUser = User;  // same fields
