765baaa50846d7c14449eaecf766765f5ad3f430
PgBiel <9021226+PgBiel@users.noreply.github.com>
2023-03-19 20:13:05 -0300

    util: improve duration->string methods

--- src/util.rs
+++ src/util.rs
@@ -10,21 +10,72 @@ use crate::error::{Error, Result};
 pub fn convert_duration_to_string(duration: std::time::Duration) -> String {
     let dur = chrono::Duration::from_std(duration);
     if let Ok(dur) = dur {
-        if dur.num_minutes() < 1 {
-            format!("{} seconds", dur.num_seconds())
-        } else if dur.num_hours() < 1 {
-            format!("{} minutes", dur.num_minutes())
-        } else if dur.num_days() < 1 {
-            format!(
-                "{} hours and {} minutes",
-                dur.num_hours(),
-                dur.num_minutes()
-            )
-        } else {
-            format!("{} days", dur.num_days())
-        }
+        convert_chrono_duration_to_string(dur)
     } else {
-        format!("{} seconds", duration.as_secs())
+        let seconds = duration.as_secs();
+        format!("{} second{}", seconds, if seconds != 1 { "s" } else { "" })
+    }
+}
+
+/// Converts a Chrono duration to a string.
+pub fn convert_chrono_duration_to_string(duration: chrono::Duration) -> String {
+    if duration.num_minutes() < 1 {
+        let seconds = duration.num_seconds();
+        format!("{} second{}", seconds, if seconds != 1 { "s" } else { "" })
+    } else if duration.num_hours() < 1 {
+        let minutes = duration.num_minutes();
+        format!("{} minute{}", minutes, if minutes != 1 { "s" } else { "" })
+    } else if duration.num_days() < 1 {
+        let hours = duration.num_hours();
+        let minutes = duration.num_minutes() % 60;
+        format!(
+            "{} hour{}{}",
+            hours,
+            if hours != 1 { "s" } else { "" },
+            if minutes > 0 {
+                format!(
+                    " and {} minute{}",
+                    minutes,
+                    if minutes != 1 { "s" } else { "" }
+                )
+            } else {
+                String::new()
+            }
+        )
+    } else {
+        let days = duration.num_days();
+        format!("{} day{}", days, if days != 1 { "s" } else { "" })
+    }
+}
+
+/// Converts a Chrono duration to a string in Brazilian Portuguese.
+pub fn convert_chrono_duration_to_brazilian_string(duration: chrono::Duration) -> String {
+    if duration.num_minutes() < 1 {
+        let seconds = duration.num_seconds();
+        format!("{} segundo{}", seconds, if seconds != 1 { "s" } else { "" })
+    } else if duration.num_hours() < 1 {
+        let minutes = duration.num_minutes();
+        format!("{} minuto{}", minutes, if minutes != 1 { "s" } else { "" })
+    } else if duration.num_days() < 1 {
+        let hours = duration.num_hours();
+        let minutes = duration.num_minutes() % 60;
+        format!(
+            "{} hora{}{}",
+            hours,
+            if hours != 1 { "s" } else { "" },
+            if minutes > 0 {
+                format!(
+                    " e {} minuto{}",
+                    minutes,
+                    if minutes != 1 { "s" } else { "" }
+                )
+            } else {
+                String::new()
+            }
+        )
+    } else {
+        let days = duration.num_days();
+        format!("{} dia{}", days, if days != 1 { "s" } else { "" })
     }
 }
 
