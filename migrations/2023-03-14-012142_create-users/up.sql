bca059d4559fbe4b78705910024c7e4a1ead4068
PgBiel <9021226+PgBiel@users.noreply.github.com>
2023-03-18 23:03:43 -0300

    fix migration user id constraint

--- migrations/2023-03-14-012142_create-users/up.sql
+++ migrations/2023-03-14-012142_create-users/up.sql
@@ -1,6 +1,6 @@
 -- Your SQL goes here
 CREATE TABLE users (
-    discord_id VARCHAR PRIMARY KEY CHECK(discord_id ~ '^\d{,20}$'),
+    discord_id VARCHAR PRIMARY KEY CHECK(discord_id ~ '^\d{1,20}$'),
     name VARCHAR NOT NULL,
     bio TEXT
 );
