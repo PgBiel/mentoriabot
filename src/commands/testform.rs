4075edc9baa2e84830c13d6021c90497087020d9
PgBiel <9021226+PgBiel@users.noreply.github.com>
2023-03-24 02:07:30 -0300

    testform cmd: re-enable

--- src/commands/testform.rs
+++ src/commands/testform.rs
@@ -1,42 +1,43 @@
-// use poise::serenity_prelude as serenity;
+use std::sync::Arc;
+use poise::serenity_prelude as serenity;
 
 use crate::{
-    common::ApplicationContext,
+    common::{ApplicationContext, Data},
     error::Error,
-    // forms::{ButtonComponent, GenerateReply, InteractionForm, SelectOption},
+    forms::{ButtonComponent, GenerateReply, InteractionForm, SelectOption},
 };
 
-// #[derive(Debug, Default, Clone, poise::Modal)]
-// #[name = "Random modal"]
-// pub struct TestFormModal {
-//     #[name = "Name"]
-//     #[placeholder = "Joseph"]
-//     #[min_length = 3]
-//     #[max_length = 20]
-//     name: String,
+#[derive(Debug, Default, Clone, poise::Modal)]
+#[name = "Random modal"]
+pub struct TestFormModal {
+    #[name = "Name"]
+    #[placeholder = "Joseph"]
+    #[min_length = 3]
+    #[max_length = 20]
+    name: String,
 
-//     #[name = "age"]
-//     #[max_length = 3]
-//     age: Option<String>,
+    #[name = "age"]
+    #[max_length = 3]
+    age: Option<String>,
 
-//     #[name = "More"]
-//     #[max_length = 500]
-//     #[paragraph]
-//     more: Option<String>,
-// }
+    #[name = "More"]
+    #[max_length = 500]
+    #[paragraph]
+    more: Option<String>,
+}
 
-// fn label_function(ctx: ApplicationContext<'_>, _: &()) -> String {
-//     format!("I am {}", ctx.author().name).into()
-// }
+fn label_function(ctx: ApplicationContext<'_>, _: &()) -> String {
+    format!("I am {}", ctx.author().name).into()
+}
 
-// #[derive(ButtonComponent, GenerateReply, Clone, Debug)]
-// #[ctx_data = "Data"]
-// #[ctx_error = "Error"]
-// #[message_content = "bruh"]
-// #[label_function = "label_function"]
-// #[danger]
-// #[message_ephemeral]
-// pub struct Button(#[interaction] serenity::MessageComponentInteraction);
+#[derive(ButtonComponent, GenerateReply, Clone, Debug)]
+#[ctx_data = "Data"]
+#[ctx_error = "Error"]
+#[message_content = "bruh"]
+#[label_function = "label_function"]
+#[danger]
+#[message_ephemeral]
+pub struct Button(#[interaction] Arc<serenity::MessageComponentInteraction>);
 
 // #[derive(SelectOption)]
 // #[ctx_data = "Data"]
@@ -46,40 +47,40 @@ use crate::{
 //     #[description = "Omg"]
 //     #[is_default]
 //     Sus,
-
+//
 //     #[label = "Oh no"]
 //     Amogus(i32, Option<u64>),
-
+//
 //     #[label = "MOMENT"]
 //     Rhombus {
 //         caustic: i32,
 //         mega: u64,
 //         test: String,
 //         wow: Option<String>,
-
+//
 //         #[initializer = "Box::new(Test::Sus)"]
 //         giga: Box<Test>,
 //     },
 // }
 
-// #[derive(InteractionForm, Debug, Clone)]
-// #[ctx_data = "Data"]
-// #[ctx_error = "Error"]
-// #[data = "()"]
-// pub struct TestForm {
-//     #[modal]
-//     modal_answers: TestFormModal,
+#[derive(InteractionForm, Debug, Clone)]
+#[ctx_data = "Data"]
+#[ctx_error = "Error"]
+#[data = "()"]
+pub struct TestForm {
+    #[modal]
+    modal_answers: TestFormModal,
 
-//     // #[component]
-//     // first_sel_comp: TestFormFirstSelection,
-//     #[component]
-//     button: Button,
-// }
+    // #[component]
+    // first_sel_comp: TestFormFirstSelection,
+    #[component]
+    button: Button,
+}
 
 /// Runs test form
 #[poise::command(slash_command)]
-pub async fn testform(_ctx: ApplicationContext<'_>) -> Result<(), Error> {
-    // let data = TestForm::execute(ctx).await?;
-    // println!("Got testform data: {:?}", data);
+pub async fn testform(ctx: ApplicationContext<'_>) -> Result<(), Error> {
+    let data = TestForm::execute(ctx).await?;
+    println!("Got testform data: {:?}", data);
     Ok(())
 }
