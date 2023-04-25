721c24ea8c39e58cd65cd6bda1da85386a2fe64c
PgBiel <9021226+PgBiel@users.noreply.github.com>
2023-04-25 13:03:09 -0300

    more time utilities

--- src/util/time.rs
+++ src/util/time.rs
@@ -1,5 +1,6 @@
 use std::fmt::Display;
 
+use chrono::{TimeZone, Timelike};
 use once_cell::sync::Lazy;
 
 pub mod parse;
@@ -14,6 +15,11 @@ pub static BRAZIL_TIMEZONE: Lazy<chrono::FixedOffset> = Lazy::new(|| {
     chrono::FixedOffset::west_opt(3 * HOUR).unwrap()
 });
 
+/// Returns the current time in the Brazil main timezone
+pub fn brazil_now() -> chrono::DateTime<chrono::FixedOffset> {
+    chrono::Utc::now().with_timezone(&*BRAZIL_TIMEZONE)
+}
+
 /// Displays some time as HOUR:MINUTE:SECOND.
 pub fn hour_minute_second_display(time: chrono::NaiveTime) -> impl Display {
     time.format("%H:%M:%S")
@@ -26,4 +32,21 @@ pub fn hour_minute_display(time: chrono::NaiveTime) -> impl Display {
 
 pub fn parse_hour_minute(string: &str) -> chrono::ParseResult<chrono::NaiveTime> {
     chrono::NaiveTime::parse_from_str(string, "%H:%M")
+}
+
+/// Replaces the time in a [`chrono::DateTime`] with a [`chrono::NaiveTime`] object's time fields.
+pub fn datetime_with_time<T: TimeZone>(
+    datetime: chrono::DateTime<T>,
+    time: chrono::NaiveTime,
+) -> Option<chrono::DateTime<T>> {
+    datetime
+        .with_hour(time.hour())
+        .and_then(|d| d.with_minute(time.minute()))
+        .and_then(|d| d.with_second(time.second()))
+}
+
+pub fn datetime_as_utc<T: TimeZone>(
+    datetime: &chrono::DateTime<T>,
+) -> chrono::DateTime<chrono::Utc> {
+    chrono::Utc.from_utc_datetime(&datetime.naive_utc())
 }
