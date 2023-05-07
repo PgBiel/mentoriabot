41d89ce593095e0311be0df55c7b181feda7c127
PgBiel <9021226+PgBiel@users.noreply.github.com>
2023-05-07 04:43:14 -0300

    calendar: initial impl for event stuff

--- src/notification/calendar.rs
+++ src/notification/calendar.rs
@@ -1,22 +1,28 @@
 //! Manages access to the Google Calendar API
 
-use google_calendar3::{api::Calendar, hyper, hyper_rustls, oauth2, CalendarHub};
+use google_calendar3::{
+    api::{Calendar, Event, EventDateTime},
+    hyper, hyper_rustls, oauth2, CalendarHub,
+};
+
+use crate::{error::Result, model::Session};
 
 /// Manages Google Calendar operations.
 pub struct CalendarManager {
     hub: CalendarHub<hyper_rustls::HttpsConnector<hyper::client::HttpConnector>>,
+    calendar_id: String,
 }
 
 impl CalendarManager {
     /// Connects to the Google Calendar API, creating a new CalendarManager instance.
-    async fn connect(secret: oauth2::ApplicationSecret) -> Self {
+    async fn connect(secret: oauth2::ApplicationSecret, calendar_id: &str) -> Result<Self> {
         let auth = oauth2::InstalledFlowAuthenticator::builder(
             secret,
             oauth2::InstalledFlowReturnMethod::HTTPRedirect,
         )
         .build()
-        .await
-        .unwrap();
+        .await?;
+
         let hub = CalendarHub::new(
             hyper::Client::builder().build(
                 hyper_rustls::HttpsConnectorBuilder::new()
@@ -29,6 +35,34 @@ impl CalendarManager {
             auth,
         );
 
-        Self { hub }
+        Ok(Self {
+            hub,
+            calendar_id: calendar_id.into(),
+        })
+    }
+
+    /// Creates a Google Calendar given a Session object.
+    async fn create_event_for_session(&self, session: &Session) -> Result<Event> {
+        let event = Event {
+            // FIXME: use translations
+            description: "Mentoria".to_string().into(),
+            start: Some(EventDateTime {
+                date_time: Some(session.start_at.clone()),
+                ..Default::default()
+            }),
+            end: Some(EventDateTime {
+                date_time: Some(session.end_at.clone()),
+                ..Default::default()
+            }),
+            ..Default::default()
+        };
+
+        self.hub
+            .events()
+            .insert(event, &self.calendar_id)
+            .doit()
+            .await
+            .map(|(_, event)| event)
+            .map_err(From::from)
     }
 }
