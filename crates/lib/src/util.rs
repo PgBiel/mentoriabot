6a4dde286668bc9a581307700c2feab24fecf4c2
PgBiel <9021226+PgBiel@users.noreply.github.com>
2023-07-04 01:45:30 -0300

    lib: validation utils

--- crates/lib/src/util.rs
+++ crates/lib/src/util.rs
@@ -4,6 +4,8 @@ pub mod iter;
 pub mod locale;
 pub mod macros;
 pub mod time;
+mod validation;
 
 pub use macros::tr;
 pub use time::{HumanParseableDateTime, BRAZIL_TIMEZONE};
+pub use validation::Unvalidated;
---
+++ crates/lib/src/util/validation.rs
@@ -0,0 +1,22 @@
+//! Utilities for validation in the bot's code.
+
+/// Indicates this value was not validated yet, and must
+/// be validated to be used.
+/// Part of the idea of making 'invalid states
+/// unrepresentable'.
+pub struct Unvalidated<T: validator::Validate>(T);
+
+impl<T: validator::Validate> Unvalidated<T> {
+    /// Creates a new 'Unvalidated' instance with
+    /// the given value. It will have to be validated
+    /// to be taken out.
+    pub fn new(value: T) -> Self {
+        Self(value)
+    }
+
+    /// Try to validate the unvalidated item, returning it
+    /// if possible, or erroring if invalid.
+    pub fn validate(self) -> Result<T, validator::ValidationErrors> {
+        self.0.validate().map(|_| self.0)
+    }
+}
