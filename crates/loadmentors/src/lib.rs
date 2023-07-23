66f3a886290ab916859e398c5ee1b8e753b17b6b
PgBiel <9021226+PgBiel@users.noreply.github.com>
2023-07-23 03:51:31 -0300

    loadmentors: load_teachers_to_db

--- crates/loadmentors/src/lib.rs
+++ crates/loadmentors/src/lib.rs
@@ -3,6 +3,8 @@
 
 mod reader;
 
+use reader::ReaderResult;
+
 /// Dependency re-exports.
 pub mod exports {
     /// Re-exports the bot's library crate.
@@ -11,9 +13,39 @@ pub mod exports {
     }
 }
 
-pub fn load(
-    csv_content: &str,
+use exports::lib::{self, db::Repository};
+
+/// Reads teachers from a CSV file and inserts them
+/// in the database.
+pub async fn load_teachers_to_db(
+    csv_contents: &str,
     database_url: &str,
-) -> exports::lib::error::Result<Vec<exports::lib::model::Teacher>> {
-    todo!()
+) -> lib::error::Result<ReaderResult<lib::model::Teacher>> {
+    let new_teachers = match reader::read_teacher_csv(csv_contents)? {
+        Ok(new_teachers) => new_teachers,
+        Err(line_errors) => return Ok(Err(line_errors)),
+    };
+
+    Ok({
+        let db = lib::db::DatabaseManager::new(database_url)?;
+        let mut teachers = Vec::new();
+        let mut errors = Vec::new();
+        teachers.reserve(new_teachers.len());
+
+        for (i, new_teacher) in new_teachers.into_iter().enumerate() {
+            if errors.len() > 5 {
+                break; // 5 errors max is good enough
+            }
+            match db.teacher_repository().insert(&new_teacher).await {
+                Ok(teacher) => teachers.push(teacher),
+                Err(db_err) => errors.push((i + 1, db_err.into())),
+            }
+        }
+
+        if errors.is_empty() {
+            Ok(teachers)
+        } else {
+            Err(errors)
+        }
+    })
 }
