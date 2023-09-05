6a724c451ac457ec61e866e6fffc9f6d6a440fc0
PgBiel <9021226+PgBiel@users.noreply.github.com>
2023-09-05 03:29:20 -0300

    nix/module: fix service permissions

--- nix/module.nix
+++ nix/module.nix
@@ -11,6 +11,8 @@ let
 
   hostSystem = pkgs.stdenv.hostPlatform.system;
   mentoriabot = flake.packages.${hostSystem}.default;
+
+  runDir = "/run/mentoriabot";
 in
 {
   options.services.mentoriabot = {
@@ -31,6 +33,23 @@ in
       default = mentoriabot;
     };
     protect = mkDisableOption "Enables sandboxing and general security measures for the bot.";
+
+    runAs.user = lib.mkOption {
+      type = with lib.types; nullOr str;
+      description = "User to run the bot as. Defaults to root (null).";
+      default = null;
+      example = "1000";
+    };
+
+    runAs.group = lib.mkOption {
+      type = with lib.types; nullOr str;
+      description = ''
+        Group of the user to run the bot as. Defaults to 100 (if a user was specified) or root otherwise
+        (equivalent to null).
+      '';
+      default = null;
+      example = "100";
+    };
   };
 
   config = lib.mkIf cfg.enable {
@@ -51,24 +70,42 @@ in
 
         Restart = "on-failure"; # restart on exit code 1 or segfault or whatever
         WorkingDirectory = workdir;
+
+        User = lib.mkIf (cfg.runAs.user != null) cfg.runAs.user;
+        Group = lib.mkIf (cfg.runAs.group != null || cfg.runAs.user != null)
+          (
+            if cfg.runAs.group != null
+            then cfg.runAs.group
+            else "100"
+          );
       } // lib.optionalAttrs cfg.protect {
         # only enable these options if 'services.mentoriabot.protect == true'
         RuntimeDirectory = "mentoriabot"; # create a /run/ directory for temp stuff
         RuntimeDirectoryMode = "0755";
         BindReadOnlyPaths = [
-          "${workdir}:/run/mentoriabot/workdir"
+          # begin: logging mounts
+          "/dev/log"
+          "/run/systemd/journal/socket"
+          "/run/systemd/journal/stdout"
+          # end: logging mounts
+          "/etc" # network stuff and whatnot
+          "/nix" # necessary to run the thing
         ];
-        WorkingDirectory = "/run/mentoriabot/workdir";
+        BindPaths = [ workdir ];
+        ReadWritePaths = [ workdir ];
+
+        # chroot into runDir
+        RootDirectory = runDir;
 
         # security options
-        DynamicUser = "yes"; # don't run as root
+        # had to remove DynamicUser as it broke writing files
         NoNewPrivileges = "yes"; # child processes won't be able to... well we don't even have those
         LockPersonality = "yes"; # we don't need whatever that is
         PrivateTmp = "yes"; # remove access to /tmp
         PrivateDevices = "yes"; # remove access to devices
 
-        ProtectHome = "yes"; # forbid home access
-        ProtectSystem = "strict"; # read-only FS
+        ProtectHome = "tmpfs"; # replace home by a RAM-bound folder
+        ProtectSystem = "strict"; # read-only FS (except for run dir)
         ProtectClock = "yes"; # read-only clock
         ProtectHostname = "yes"; # not like we need that info
         ProtectKernelLogs = "yes"; # why would we access that
