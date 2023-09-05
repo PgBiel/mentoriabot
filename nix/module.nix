8f8ce9ddd2e403c3f37b2e7268becaffbf1ad680
PgBiel <9021226+PgBiel@users.noreply.github.com>
2023-09-05 01:26:28 -0300

    restore protect home and system thanks to readwritepaths

--- nix/module.nix
+++ nix/module.nix
@@ -33,6 +33,23 @@ in
       default = mentoriabot;
     };
     protect = mkDisableOption "Enables sandboxing and general security measures for the bot.";
+
+    runAs.user = lib.mkOption {
+      type = with lib.types; nullOr str;
+      description = "User to run the bot as. Defaults to root (null).";
+      default = null;
+      example = "1000";
+    };
+
+    runAs.group = lib.mkOption {
+      type = with lib.types; nullOr str;
+      description = ''
+        Group of the user to run the bot as. Defaults to 100 (if a user was specified) or root otherwise
+        (equivalent to null).
+      '';
+      default = null;
+      example = "100";
+    };
   };
 
   config = lib.mkIf cfg.enable {
@@ -53,12 +70,20 @@ in
 
         Restart = "on-failure"; # restart on exit code 1 or segfault or whatever
         WorkingDirectory = workdir;
+
+        User = lib.mkIf (cfg.runAs.user != null) cfg.runAs.user;
+        Group = lib.mkIf (cfg.runAs.group != null || cfg.runAs.user != null)
+          (
+            if cfg.runAs.group != null
+            then cfg.runAs.group
+            else "100"
+          );
       } // lib.optionalAttrs cfg.protect {
         # only enable these options if 'services.mentoriabot.protect == true'
         RuntimeDirectory = "mentoriabot"; # create a /run/ directory for temp stuff
         RuntimeDirectoryMode = "0755";
         BindPaths = [
-          "${workdir}"
+          workdir
         ];
         BindReadOnlyPaths = [
           # begin: logging mounts
@@ -69,12 +94,15 @@ in
           "/etc"
           "/nix"
         ];
+        ReadWritePaths = workdir;
 
         # chroot into runDir
-        RootDirectory = "${runDir}";
+        RootDirectory = runDir;
 
         # security options
         # had to remove DynamicUser as it broke writing files
+        ProtectHome = "tmpfs"; # replace home by a RAM-bound folder
+        ProtectSystem = "strict"; # read-only FS (except for run dir)
         NoNewPrivileges = "yes"; # child processes won't be able to... well we don't even have those
         LockPersonality = "yes"; # we don't need whatever that is
         PrivateTmp = "yes"; # remove access to /tmp
