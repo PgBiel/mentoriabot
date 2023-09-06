ad2780d5dcf78dec2309878e40fd554bb38f1c7b
PgBiel <9021226+PgBiel@users.noreply.github.com>
2023-09-06 02:20:03 -0300

    lib(util/iter): add sort by reference key

--- crates/lib/src/util/iter.rs
+++ crates/lib/src/util/iter.rs
@@ -42,11 +42,43 @@ pub fn count_unique<T: Eq + Hash>(objects: impl IntoIterator<Item = T>) -> usize
     objects.into_iter().collect::<HashSet<_>>().len()
 }
 
+/// Compares two objects applying a key function which returns a reference.
+fn compare_reference_keys<T, R>(a: &T, b: &T, key: &mut impl FnMut(&T) -> &R) -> std::cmp::Ordering
+where
+    for<'a> &'a R: Ord,
+{
+    let ak = key(a);
+    let bk = key(b);
+    ak.cmp(&bk)
+}
+
+/// Sorts a vector by a key function that returns a reference.
+/// Useful when sorting strings.
+/// See https://stackoverflow.com/a/47126516
+pub fn sort_by_reference_key<T, R>(v: &mut [T], mut key: impl FnMut(&T) -> &R)
+where
+    for<'a> &'a R: Ord,
+{
+    v.sort_by(|a, b| compare_reference_keys(a, b, &mut key));
+}
+
 #[cfg(test)]
 mod tests {
     use std::collections::HashSet;
 
+    use super::sort_by_reference_key;
     use crate::util::iter::{count_unique, group_by, group_by_count};
+
+    #[derive(Debug, Clone, PartialEq, Eq)]
+    struct Animal {
+        name: String,
+    }
+
+    impl Animal {
+        fn new(name: impl Into<String>) -> Self {
+            Self { name: name.into() }
+        }
+    }
 
     #[test]
     fn test_group_by_correctly_groups_up_numbers_by_mod_4() {
@@ -88,5 +120,27 @@ mod tests {
         let values = [1, 1, 2, 2, 2, 2, 2, 3, 4, 6, 10];
 
         assert_eq!(6, count_unique(values));
+    }
+
+    #[test]
+    fn test_sort_by_reference_key_works_with_string_keys() {
+        let mut animals = vec![
+            Animal::new("ccc"),
+            Animal::new("ddd"),
+            Animal::new("aaa"),
+            Animal::new("eee"),
+            Animal::new("bbb"),
+        ];
+        sort_by_reference_key(&mut animals, |animal| &animal.name);
+        assert_eq!(
+            animals,
+            vec![
+                Animal::new("aaa"),
+                Animal::new("bbb"),
+                Animal::new("ccc"),
+                Animal::new("ddd"),
+                Animal::new("eee")
+            ]
+        );
     }
 }
