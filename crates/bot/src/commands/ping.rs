3023ccf563bf8090e116a9480d4b46d8bce9d20f
PgBiel <9021226+PgBiel@users.noreply.github.com>
2023-09-07 12:48:00 -0300

    bot(cmds/ping): update command description

--- crates/bot/src/commands/ping.rs
+++ crates/bot/src/commands/ping.rs
@@ -1,7 +1,11 @@
 use crate::{common::Context, lib::error::Error};
 
-/// Displays your or another user's account creation date
-#[poise::command(slash_command)]
+/// Checks if the bot is alive
+#[poise::command(
+    slash_command,
+    ephemeral,
+    description_localized("pt-BR", "Verifica se o bot está funcionando.")
+)]
 pub async fn ping(ctx: Context<'_>) -> Result<(), Error> {
     ctx.send(|reply| reply.content("Pong!").ephemeral(true))
         .await?;
